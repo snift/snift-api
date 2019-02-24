2330f6ff7cb3cb81b4a74d5c61d618c28c897e1e
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-02-24 23:21:20 +0530

    Added api and functinal error handling, removed unused utils
    - Replaced utils.splitHostPort with the inbuilt method
    - Made GetCertificate aware of protocol its handling to avoid tls overloads
    - Updated GetCertificate tests, removed unused tests

--- models/certificate.go
+++ models/certificate.go
@@ -3,8 +3,8 @@ package models
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"net"
-	"snift-backend/utils"
 	"time"
 )
 
@@ -20,14 +20,14 @@ type Cert struct {
 	SANs       []string `json:"sans"`
 	NotBefore  string   `json:"not_before"`
 	NotAfter   string   `json:"not_after"`
-	Error      string   `json:"error"`
 	certChain  []*x509.Certificate
 }
 
-var serverCert = func(host, port string) ([]*x509.Certificate, string, error) {
+var serverCert = func(host string, port string) ([]*x509.Certificate, string, error) {
 	d := &net.Dialer{
 		Timeout: time.Duration(TimeoutSeconds) * time.Second,
 	}
+	fmt.Println(host + " " + port)
 	conn, err := tls.DialWithDialer(d, "tcp", host+":"+port, &tls.Config{
 		InsecureSkipVerify: false,
 	})
@@ -44,14 +44,15 @@ var serverCert = func(host, port string) ([]*x509.Certificate, string, error) {
 }
 
 // GetCertificate returns the Certificate associated with a host-port
-func GetCertificate(hostport string) *Cert {
-	host, port, err := utils.SplitHostPort(hostport)
-	if err != nil {
-		return &Cert{DomainName: host, Error: err.Error()}
+func GetCertificate(host string, port string, protocol string) (*Cert, error) {
+	// dont get certificates for non-https protocols, and when port number is 80
+	// trying to fetch certs with port:80 causes tls overload
+	if protocol != "https" || (protocol == "https" && port == "80") {
+		return nil, nil
 	}
 	certChain, ip, err := serverCert(host, port)
 	if err != nil {
-		return &Cert{DomainName: host, Error: err.Error()}
+		return &Cert{DomainName: host}, err
 	}
 	cert := certChain[0]
 
@@ -65,7 +66,6 @@ func GetCertificate(hostport string) *Cert {
 		SANs:       cert.DNSNames, // Subject Alternative Name
 		NotBefore:  cert.NotBefore.In(loc).String(),
 		NotAfter:   cert.NotAfter.In(loc).String(),
-		Error:      "",
 		certChain:  certChain,
-	}
+	}, nil
 }
--- models/certificate_test.go
+++ models/certificate_test.go
@@ -2,15 +2,21 @@ package models
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetCertificates(t *testing.T) {
-	results := GetCertificate("example.com")
+	// returns certificates for valid https urls
+	results, error := GetCertificate("example.com", "443", "https")
+	assert.Nil(t, error)
 	assert.Equal(t, results.DomainName, "example.com", "Domain Names should be equal")
 	assert.NotEmpty(t, results.certChain)
 	assert.NotEmpty(t, results.CommonName)
 	assert.NotEmpty(t, results.Issuer)
 	assert.NotEmpty(t, results.SANs)
-	assert.Empty(t, results.Error)
+	// returns nil for non-https endpoints
+	results, error = GetCertificate("example.com", "80", "http")
+	assert.Nil(t, results)
+	assert.Nil(t, error)
 }
