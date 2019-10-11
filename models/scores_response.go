3465d446d03d32b133d27f0900c38d550c5bc91e
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-12 01:56:11 +0530

    rename: ScoresResponse struct and func

--- models/scores_response.go
+++ models/scores_response.go
@@ -1,16 +1,16 @@
 package models
 
-// ScoreResponse holds a Score JSON, the Certificate Details JSON for the main Scores API
-type ScoreResponse struct {
+// ScoresResponse holds a Score JSON, the Certificate Details JSON for the main Scores API
+type ScoresResponse struct {
 	Scores       *Scores       `json:"scores"`
 	Cert         *Cert         `json:"certificate_details,omitempty"`
 	IncidentList []Incident    `json:"security_incidents,omitempty"`
 	ServerDetail *ServerDetail `json:"web_server,omitempty"`
 }
 
-// GetScoresResponse returns a main Scores Response
-func GetScoresResponse(scores *Scores, cert *Cert, IncidentList []Incident, ServerDetail *ServerDetail) *ScoreResponse {
-	response := &ScoreResponse{
+// BuildScoresResponse builds the final api response for /score
+func BuildScoresResponse(scores *Scores, cert *Cert, IncidentList []Incident, ServerDetail *ServerDetail) *ScoresResponse {
+	response := &ScoresResponse{
 		Scores:       scores,
 		Cert:         cert,
 		IncidentList: IncidentList,
