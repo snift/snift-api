49e2125863bb14c90d97e339162aae960845253c
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-24 02:23:43 +0530

    Add category to badges

--- models/badge.go
+++ models/badge.go
@@ -2,6 +2,7 @@ package models
 
 // Badge holds a showpiece to represent domain information
 type Badge struct {
-	Name    string `json:"name"`
-	Message string `json:"message"`
+	Name     string `json:"name"`
+	Message  string `json:"message"`
+	Category string `json:"category"`
 }
